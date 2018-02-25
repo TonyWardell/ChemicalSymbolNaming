cbc76359bc356bc6e49b6299e607b9032c311765
TonyWardell <tonyandamie@yahoo.co.uk>
2018-02-25 11:37:06 +0000

    Make member variable private.

--- src/test/java/uk/wardell/tony/core/strategy/CandidateNameEvaluatorTest.java
+++ src/test/java/uk/wardell/tony/core/strategy/CandidateNameEvaluatorTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class CandidateNameEvaluatorTest {
-    CandidateNameEvaluator candidateNameEvaluator;
+    private CandidateNameEvaluator candidateNameEvaluator;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
@@ -16,11 +16,44 @@ class CandidateNameEvaluatorTest {
     }
 
     @org.junit.jupiter.api.Test
-    void evaluate() {
-
+    void evaluate0() {
         CandidateName candidateName = new CandidateName("Spenglerium","Ee");
         NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
         assertThat(namingResult.isResult(), is(true));
+    }
 
+    @org.junit.jupiter.api.Test
+    void evaluate1() {
+        CandidateName candidateName = new CandidateName("Zeddemorium","Zr");
+        NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
+        assertThat(namingResult.isResult(), is(true));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluate2() {
+        CandidateName candidateName = new CandidateName("Venkmine","Kn");
+        NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
+        assertThat(namingResult.isResult(), is(true));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluate3() {
+        CandidateName candidateName = new CandidateName("Stantzon","Zt");
+        NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
+        assertThat(namingResult.isResult(), is(false));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluate4() {
+        CandidateName candidateName = new CandidateName("Melintzum","Nn");
+        NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
+        assertThat(namingResult.isResult(), is(false));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluate5() {
+        CandidateName candidateName = new CandidateName("Tullium","Ty");
+        NamingResult namingResult = candidateNameEvaluator.evaluate(candidateName);
+        assertThat(namingResult.isResult(), is(false));
     }
 }
