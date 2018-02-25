0300584f3f3257b1f4d637eb4788950ee53a0ceb
TonyWardell <tonyandamie@yahoo.co.uk>
2018-02-25 11:37:14 +0000

    Rename variables.

--- src/main/java/uk/wardell/tony/core/strategy/CandidateNameEvaluator.java
+++ src/main/java/uk/wardell/tony/core/strategy/CandidateNameEvaluator.java
@@ -14,18 +14,17 @@ public class CandidateNameEvaluator {
         char symChar1 = symbolLc.charAt(0);
         char symChar2 = symbolLc.charAt(1);
 
+        int c1Location = fullNameLc.indexOf(symChar1);
 
-        int c1 = fullNameLc.indexOf(symChar1);
-
-        if (c1 == fullNameLc.length() - 1) {
+        if (c1Location == fullNameLc.length() - 1) {
             return new NamingResult(false);
         }
 
-        int c2 = fullNameLc.indexOf(symChar2, c1 + 1);
-        boolean c1b = c1 > -1;
-        boolean c2b = c2 > -1;
-        boolean different = c1 != c2;
+        int c2Location = fullNameLc.indexOf(symChar2, c1Location + 1);
+        boolean c1Found = c1Location > -1;
+        boolean c2Found = c2Location > -1;
+        boolean different = c1Location != c2Location;
 
-        return new NamingResult(c1b && c2b && different);
+        return new NamingResult(c1Found && c2Found && different);
     }
 }
