72abdb420041fed5bbe75c707735c327f007aa3d
TonyWardell <tonyandamie@yahoo.co.uk>
2018-11-24 21:09:29 +0000

    Make final

--- src/main/java/uk/wardell/tony/chemicalnaming/terse/NamingResult.java
+++ src/main/java/uk/wardell/tony/chemicalnaming/terse/NamingResult.java
@@ -1,7 +1,7 @@
 package uk.wardell.tony.chemicalnaming.terse;
 
 public class NamingResult {
-    final boolean result;
+    private final boolean result;
     public NamingResult(boolean b) {
         result = b;
     }
