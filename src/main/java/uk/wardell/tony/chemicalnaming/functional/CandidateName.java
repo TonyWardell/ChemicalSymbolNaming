7dd4de8b1fa04cca886c11de4e67e8f5d4c87663
TonyWardell <tonyandamie@yahoo.co.uk>
2018-11-24 07:21:11 +0000

    Make field access stricter.

--- src/main/java/uk/wardell/tony/chemicalnaming/functional/CandidateName.java
+++ src/main/java/uk/wardell/tony/chemicalnaming/functional/CandidateName.java
@@ -1,8 +1,8 @@
 package uk.wardell.tony.chemicalnaming.functional;
 
 public class CandidateName {
-    public final String element;
-    public final String symbol;
+    final String element;
+    final String symbol;
 
     public CandidateName(String name, String symbol) {
         this.element = name;
