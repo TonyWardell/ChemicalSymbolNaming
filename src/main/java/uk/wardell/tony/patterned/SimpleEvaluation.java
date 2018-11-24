857253b84f1cd25ce6c752e117e31c2cebfe0343
TonyWardell <tonyandamie@yahoo.co.uk>
2018-11-24 09:07:19 +0000

    Generify.

---
+++ src/main/java/uk/wardell/tony/patterned/SimpleEvaluation.java
@@ -0,0 +1,12 @@
+package uk.wardell.tony.patterned;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class SimpleEvaluation<T,R> implements Evaluation<T,R>{
+
+    public Function<T, R> create(Predicate<T> predicate, R success, R failure){
+
+        return target -> predicate.test(target) ? success : failure;
+    }
+}
