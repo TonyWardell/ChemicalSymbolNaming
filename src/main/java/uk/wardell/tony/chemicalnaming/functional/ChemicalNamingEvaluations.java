bcedff3a7d18c0dc3ca268f2b519e0e632d74d66
TonyWardell <tonyandamie@yahoo.co.uk>
2018-11-24 19:56:37 +0000

    Simplify logic to conduct tests. Make nature of evaluated predicate clearer.

--- src/main/java/uk/wardell/tony/chemicalnaming/functional/ChemicalNamingEvaluations.java
+++ src/main/java/uk/wardell/tony/chemicalnaming/functional/ChemicalNamingEvaluations.java
@@ -2,7 +2,8 @@ package uk.wardell.tony.chemicalnaming.functional;
 
 import uk.wardell.tony.chemicalnaming.CandidateName;
 import uk.wardell.tony.patterned.Evaluation;
-import uk.wardell.tony.patterned.SimpleEvaluation;
+import uk.wardell.tony.patterned.EvaluateSuccess;
+import uk.wardell.tony.patterned.EvaluateFailure;
 import uk.wardell.tony.patterned.response.Response;
 
 import java.util.Arrays;
@@ -14,53 +15,32 @@ import static uk.wardell.tony.chemicalnaming.NamingEvaluationResponses.*;
 
 class ChemicalNamingEvaluations {
 
-    static Function<CandidateName,Character> firstChar = cn -> cn.getSymbol().toLowerCase().charAt(0);
-    static Function<CandidateName,Character> secondChar = cn -> cn.getSymbol().toLowerCase().charAt(1);
+    static Function<CandidateName,String> firstChar = cn -> String.valueOf(cn.getSymbol().toLowerCase().charAt(0));
+    static Function<CandidateName,String> secondChar = cn -> String.valueOf(cn.getSymbol().toLowerCase().charAt(1));
 
 
     static Predicate<CandidateName> doesSymbolContainTwoLetters() {
-        return candidateName -> candidateName.getSymbol().length() == 2;
+        return cn -> cn.getSymbol().length() == 2;
     }
 
-    private static Predicate<CandidateName> ifSymbolNameIsTwoOfTheSameLetterThenNameShouldHaveThatLetterTwice() {
+    private static Predicate<CandidateName> charFrequencyInSymbolShouldMatchThatInElement() {
         return cn -> {
-            char low0 = firstChar.apply(cn);
-            char low1 = secondChar.apply(cn);
-            if(low0 != low1) return true;
-
-            //Characters are the same
-            String lower = cn.getElement().toLowerCase();
-            int loc0 = lower.indexOf(low0);
-            int loc1 = lower.lastIndexOf(low1);
-            return loc0 != loc1;
+            CharLocations charLocations = getCharLocations(cn);
+            return charLocations.loc0 != charLocations.loc1;
         };
     }
 
     private static Predicate<CandidateName> areBothSymbolLettersInTheElementName() {
-        return cn -> (cn.getElement().toLowerCase().contains("" + firstChar.apply(cn)) && cn
-                .getElement().toLowerCase().contains(("" + secondChar.apply(cn))));
+        return cn -> {
+            CharLocations charLocations = getCharLocations(cn);
+            return charLocations.loc0 != -1 && charLocations.loc1 != -1;
+        };
     }
 
     private static Predicate<CandidateName> areTheLettersInElementOrder() {
         return cn -> {
-            int firstCharacterLocation = cn.getElement().indexOf(cn.getSymbol().toLowerCase().charAt(0));
-            String restOfSequence = cn.getElement().substring(firstCharacterLocation + 1);
-            return restOfSequence.contains("" + secondChar.apply(cn));
-        };
-    }
-
-    private static Predicate<CandidateName> areAllTheLettersFromElementName() {
-        return cn -> {
-            int firstCharacterLocation = cn.getElement().indexOf(cn.getSymbol().charAt(0));
-            String restOfSequence = cn.getElement().substring(firstCharacterLocation + 1);
-
-            Character secondCharOfSymbol = secondChar.apply(cn);
-
-            if (!restOfSequence.contains("" + secondCharOfSymbol)) {
-                return !cn.getElement().contains("" + secondCharOfSymbol);
-            }
-
-            return true;
+            CharLocations charLocations = getCharLocations(cn);
+            return charLocations.loc0 < charLocations.loc1;
         };
     }
 
@@ -72,24 +52,41 @@ class ChemicalNamingEvaluations {
     }
 
     private static Predicate<CandidateName> isSecondCharacterInLowerCase(){
-        return candidateName -> {
-            char c1 = candidateName.getSymbol().charAt(1);
-            return Character.toLowerCase(c1) == c1;
+        return cn -> {
+            String c1 = String.valueOf(cn.getSymbol().charAt(1));
+            return secondChar.apply(cn).equals(c1);
         };
     }
 
 
     static List<Function<CandidateName, Response>> evaluations(){
-        Evaluation<CandidateName, Response> simpleEvaluation = new SimpleEvaluation<>();
+        Evaluation<CandidateName, Response> requireTrue = new EvaluateSuccess<>();
+        Evaluation<CandidateName, Response> requireFalse = new EvaluateFailure<>();
+
         return Arrays.asList(
-                simpleEvaluation.create(ifSymbolNameIsTwoOfTheSameLetterThenNameShouldHaveThatLetterTwice(), VALID, IF_LETTERS_IN_SYMBOL_ARE_THE_SAME_THEN_THE_NAME_SHOULD_HAVE_THOSE_LETTERS_TWICE),
-                simpleEvaluation.create(areTheLettersInElementOrder(), VALID, SYMBOL_LETTERS_NOT_IN_ORDER),
-                simpleEvaluation.create(areBothSymbolLettersInTheElementName(), VALID, SYMBOL_LETTERS_ARE_MISSING_FROM_THE_NAME),
-                simpleEvaluation.create(doesSymbolContainTwoLetters(), VALID, SYMBOL_IS_NOT_TWO_LETTERS),
-                simpleEvaluation.create(areAllTheLettersFromElementName(), VALID, SYMBOL_LETTERS_NOT_ALL_FROM_CHEM_NAME),
-                simpleEvaluation.create(isFirstCharacterInUpperCase(), VALID, FIRST_CHAR_OF_SYMBOL_NOT_UPPER_CASE),
-                simpleEvaluation.create(isSecondCharacterInLowerCase(), VALID, SECOND_CHAR_OF_SYMBOL_NOT_LOWERCASE)
+                requireTrue.create(doesSymbolContainTwoLetters(), VALID, SYMBOL_IS_NOT_TWO_LETTERS),
+                requireTrue.create(areBothSymbolLettersInTheElementName(), VALID, SYMBOL_LETTERS_ARE_MISSING_FROM_THE_NAME),
+                requireTrue.create(charFrequencyInSymbolShouldMatchThatInElement(), VALID, IF_LETTERS_IN_SYMBOL_ARE_THE_SAME_THEN_THE_NAME_SHOULD_HAVE_THOSE_LETTERS_TWICE),
+                requireTrue.create(areTheLettersInElementOrder(), VALID, SYMBOL_LETTERS_NOT_IN_ORDER),
+                requireTrue.create(isFirstCharacterInUpperCase(), VALID, FIRST_CHAR_OF_SYMBOL_NOT_UPPER_CASE),
+                requireTrue.create(isSecondCharacterInLowerCase(), VALID, SECOND_CHAR_OF_SYMBOL_NOT_LOWERCASE)
         );
     }
 
+    private static CharLocations getCharLocations(CandidateName cn) {
+        String lower = cn.getElement().toLowerCase();
+        String low0 = firstChar.apply(cn);
+        String low1 = secondChar.apply(cn);
+        return new CharLocations(lower.indexOf(low0), lower.lastIndexOf(low1));
+    }
+
+    static class CharLocations {
+        int loc0;
+        int loc1;
+
+        public CharLocations(int loc0, int loc1) {
+            this.loc0 = loc0;
+            this.loc1 = loc1;
+        }
+    }
 }
