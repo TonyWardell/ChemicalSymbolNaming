9973f812d1ab95cdf30083f9d6c79990d1cc30f7
TonyWardell <tonyandamie@yahoo.co.uk>
2018-12-24 21:33:33 +0000

    Re-jig evaluations predicate now that it requires an instance of evaluations as the checkValidity method is no longer static.

--- src/test/java/uk/wardell/tony/chemicalnaming/functional/EvaluationsTest.java
+++ src/test/java/uk/wardell/tony/chemicalnaming/functional/EvaluationsTest.java
@@ -1,25 +1,85 @@
 package uk.wardell.tony.chemicalnaming.functional;
 
-import org.junit.Test;
 import uk.wardell.tony.chemicalnaming.model.CandidateName;
+import uk.wardell.tony.patterned.response.Response;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static uk.wardell.tony.chemicalnaming.functional.Evaluations.*;
+import static uk.wardell.tony.chemicalnaming.model.NamingEvaluationResponses.*;
 
-public class EvaluationsTest {
+class EvaluationsTest {
 
-    @Test
-    public void doesSymbolContainTwoLettersIsTrue(){
-        CandidateName candidateName = new CandidateName(null, "xn");
+     Evaluations evaluations = new Evaluations();
 
-        assertThat(doesSymbolContainTwoLetters().test(candidateName), is(true));
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol0() {
+        CandidateName candidateName = new CandidateName("Spenglerium","Ee");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(VALID));
     }
 
-    @Test
-    public void doesSymbolContainTwoLettersIsFalse(){
-        CandidateName candidateName = new CandidateName(null, "sss");
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol1() {
+        CandidateName candidateName = new CandidateName("Zeddemorium","Zr");
 
-        assertThat(doesSymbolContainTwoLetters().test(candidateName), is(false));
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(VALID));    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol2() {
+        CandidateName candidateName = new CandidateName("Venkmine","Kn");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(VALID));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol3() {
+        CandidateName candidateName = new CandidateName("Melintzum","M");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(SYMBOL_IS_NOT_TWO_LETTERS));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol4() {
+        CandidateName candidateName = new CandidateName("Melintzum","Nn");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(IF_LETTERS_IN_SYMBOL_ARE_THE_SAME_THEN_THE_NAME_SHOULD_HAVE_THOSE_LETTERS_TWICE));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol5() {
+        CandidateName candidateName = new CandidateName("Tullium","uT");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(SYMBOL_LETTERS_NOT_IN_ORDER));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol6() {
+        CandidateName candidateName = new CandidateName("Tullium","tu");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(FIRST_CHAR_OF_SYMBOL_NOT_UPPER_CASE));
+    }
+
+    @org.junit.jupiter.api.Test
+    void evaluateSymbol7() {
+        CandidateName candidateName = new CandidateName("Tullium","TU");
+
+        Response response = evaluations.checkValidity(candidateName);
+
+        assertThat(response, is(SECOND_CHAR_OF_SYMBOL_NOT_LOWERCASE));
     }
 }
