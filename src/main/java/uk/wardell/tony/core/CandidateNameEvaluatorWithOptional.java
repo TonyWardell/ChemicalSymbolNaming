e592af198b1a3a51677504bd4b192b264afc065e
TonyWardell <tonyandamie@yahoo.co.uk>
2018-12-24 21:14:41 +0000

    Move candidate name to sub-package model. Re-organized code.

--- src/main/java/uk/wardell/tony/core/CandidateNameEvaluatorWithOptional.java
+++ src/main/java/uk/wardell/tony/core/CandidateNameEvaluatorWithOptional.java
@@ -2,7 +2,7 @@ package uk.wardell.tony.core;
 
 import java.util.Optional;
 
-import uk.wardell.tony.chemicalnaming.CandidateName;
+import uk.wardell.tony.chemicalnaming.model.CandidateName;
 import uk.wardell.tony.chemicalnaming.terse.NamingResult;
 import uk.wardell.tony.core.strategy.logic.Finder;
 
@@ -10,9 +10,12 @@ class CandidateNameEvaluatorWithOptional {
 
     NamingResult evaluate(CandidateName candidateName) {
 
-        Optional<Integer> found1 = Finder.find(candidateName.getElement().toLowerCase(), candidateName.getSymbol().toLowerCase().charAt(0));
-        Optional<Integer> found2 =
-                Finder.findLast(candidateName.getElement().toLowerCase(), candidateName.getSymbol().toLowerCase().charAt(1));
+        String asLowerCase = candidateName.getElement().toLowerCase();
+        char element1 = candidateName.getSymbol().toLowerCase().charAt(1);
+        char element0 = candidateName.getSymbol().toLowerCase().charAt(0);
+
+        Optional<Integer> found1 = Finder.find(asLowerCase, element0);
+        Optional<Integer> found2 = Finder.findLast(asLowerCase, element1);
 
         return new NamingResult(found1.isPresent() && found2.isPresent() && found1.get() < found2.get());
 
